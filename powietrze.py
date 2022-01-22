1fec76c954189e0aca806c5653730bb827f6cdf0
Damian Melniczuk <damian.melniczuk@gmail.com>
2022-01-22 21:50:46 +0100

    update

--- powietrze.py
+++ powietrze.py
@@ -10,6 +10,7 @@ DATA_URL = ('http://80.211.245.168/pm2_5')
 @st.cache
 def load_data():
     data = pd.read_csv(DATA_URL, index_col=0)
+    data.set_index(pd.to_datetime(data.index).tz_convert('Europe/Warsaw'), inplace=True)
     return data
 
 # Create a text element and let the reader know the data is loading.
@@ -22,3 +23,14 @@ data_load_state.text("Done! (using st.cache)")
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Meszno', 'Wójcice', 'Kałków', 'Maciejowice', 'Rynek', 'Krakowska'],
+     ['Rynek'])
+
+st.write('You selected:', options)
+
+filtered_data = data[options]
+
+st.line_chart(data=filtered_data.interpolate(method="time"), width=0, height=0, use_container_width=True)
